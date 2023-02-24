ca32b6943cc69aa49738b3e90a9cde89cb6f2f6a
ShawnPConroy <shawn@snowbank.ca>
2023-02-24 15:20:03 -0500

    update

---
+++ globasa-quark-theme.php
@@ -0,0 +1,33 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Grav; // used to add Quark as a Twig namespace (below)
+use Grav\Common\Theme;
+
+class GlobasaQuarkTheme extends Quark
+{
+    /*
+    Add Quark as a Twig namespace
+    to be able to extend that theme's individual blocks
+
+    From:
+    https://learn.getgrav.org/16/cookbook/twig-recipes#extend-base-template-of-i
+    */
+    public static function getSubscribedEvents() {
+        return [
+            'onTwigLoader' => ['onTwigLoader', 10]
+        ];
+    }
+
+    public function onTwigLoader() {
+        parent::onTwigLoader();
+
+        // add quark theme as namespace to twig
+        $quark_path = Grav::instance()['locator']->findResource('themes://quark');
+        $this->grav['twig']->addPath($quark_path . DIRECTORY_SEPARATOR . 'templates', 'quark');
+    }
+    /* end add Quark as a Twig namespace */
+
+
+    // Access plugin events in this class
+}
